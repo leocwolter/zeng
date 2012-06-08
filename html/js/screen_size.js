8a09a1fcbf8ce37bafbbda4fbdd3db7ed6dde98c
Leonardo Wolter <leocwolter@gmail.com>
2012-06-08 01:30:01 -0300

    Comentando linhas do script

--- html/js/screen_size.js
+++ html/js/screen_size.js
@@ -1,16 +1,19 @@
 // JavaScript Document
 
 $(document).ready(function(){
+	//Get Width of elements on the load
 	var projectSidebarWidth = $('#project_sidebar').width();
 	var projectSidebarClickAreaWidth = $('#project_sidebar_click_area').width();
+	
 	//Set the header, menu_bar, content right padding
 	adjustsSizes();
 
+	//Readjusts size of elements on resize event.
 	$(window).resize(function(){
 		adjustsSizes();
 	});
 	
-	
+	//Minimize/Maximize Sidebar and readjusts elements
 	$('#project_sidebar_click_area').toggle(
 		function(){
 			$('#project_sidebar').animate({'width':projectSidebarClickAreaWidth}, function(){

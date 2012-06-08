8060771b5455484b9cdb4a0593d4049862599e59
Leonardo Wolter <leocwolter@gmail.com>
2012-06-07 22:22:43 -0300

    reajustando tamanhos quando a janela é redimencionada

--- html/js/screen_size.js
+++ html/js/screen_size.js
@@ -1,22 +1,42 @@
 // JavaScript Document
 
 $(document).ready(function(){
-	
-	//Take the screen width
-	var left_container = $('#left_container').width();
-	
-	//Set the header, menu_bar, content right padding
-	$('header, #menu_bar, #content').css({'width':left_container - 251, 'padding':'0px 251px 0px 0px'});
+	$(window).resize(function(){
+		adjustsSizes();
+	});
 	
 	var projectSidebarWidth = $('#project_sidebar').width();
-	var projectSidebarContentWidth = $('#project_sidebar_content').width();
+	
+	//Set the header, menu_bar, content right padding
+	adjustsSizes();
 	
 	$('#project_sidebar_click_area').toggle(
 		function(){
-			$('#project_sidebar').animate({'width':'10px'});
+			$('#project_sidebar').animate({'width':'10px'}, function(){
+				
+				adjustsSizes();
+				
+			});
 		},function(){
-			$('#project_sidebar').animate({'width':projectSidebarWidth});				
+			$('#project_sidebar').animate({'width':projectSidebarWidth}, function(){
+				adjustsSizes();
+			});				
 		}
 	);
+	
+	
+	function adjustsSizes(){
+		var leftContainerWidth = $('#left_container').width();
+		var projectSidebarWidth = $('#project_sidebar').width();
+		var projectSidebarContentWidth = $('#project_sidebar_content').width();		
+		
+		var menuWidth = $('#menu').width();
+		
+		var width = leftContainerWidth - projectSidebarWidth;
+		var padding = projectSidebarWidth+2;
+		
+		$('header, #menu_bar, #content').css({'width':width, 'padding-right':padding});	
+	}
+	
 });
 	

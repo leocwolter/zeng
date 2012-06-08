c8e1a2987234bb39485f8ba8fb0f10201b58ce1a
Leonardo Wolter <leocwolter@gmail.com>
2012-06-07 21:34:55 -0300

    refatorando a funcao jquery

--- html/js/screen_size.js
+++ html/js/screen_size.js
@@ -3,26 +3,20 @@
 $(document).ready(function(){
 	
 	//Take the screen width
-	left_container = $('#left_container').width();
+	var left_container = $('#left_container').width();
 	
 	//Set the header, menu_bar, content right padding
 	$('header, #menu_bar, #content').css({'width':left_container - 251, 'padding':'0px 251px 0px 0px'});
 	
-	$('#project_sidebar_click_area').click(
-		function(){
-			$('#project_sidebar').animate({ width:'0px'});
-			$('#project_sidebar_click_area_return').show();
-			
-			//Set the header, menu_bar, content right padding
-			$('header, #menu_bar, #content').css({'width':left_container - 12, 'padding':'0px 12px 0px 0px'});
-
-		}
-	);
+	var projectSidebarWidth = $('#project_sidebar').width();
+	var projectSidebarContentWidth = $('#project_sidebar_content').width();
 	
-	$('#project_sidebar_click_area_return').click(
+	$('#project_sidebar_click_area').toggle(
 		function(){
-			$('#project_sidebar').animate({ width:'250px'});
-			$('#project_sidebar_click_area_return').hide();
+			$('#project_sidebar').animate({'width':'10px'});
+		},function(){
+			$('#project_sidebar').animate({'width':projectSidebarWidth});				
 		}
 	);
 });
+	

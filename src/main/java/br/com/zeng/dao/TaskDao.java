e877986ef42fc6461dcc487aaa64b0501c2d1301
Enzo Toshio <enzo.toshiba@gmail.com>
2012-07-07 23:59:13 -0300

    Criação da método getListTaskWithNameAndDescription
    Método responsável por formular / realizar a query de busca de tarefas.

--- src/main/java/br/com/zeng/dao/TaskDao.java
+++ src/main/java/br/com/zeng/dao/TaskDao.java
@@ -1,8 +1,12 @@
 package br.com.zeng.dao;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 
 import br.com.caelum.vraptor.ioc.Component;
+import br.com.zeng.model.Project;
 import br.com.zeng.model.Task;
 
 @Component
@@ -23,6 +27,16 @@ public class TaskDao {
 
 	public Task getTaskWithId(Long id) {
 		return (Task) session.get(Task.class, id);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Task> getListTaskWithNameAndDescription(String name, String description) {
+		//return (List<Task>) session.get(Task.class, name);
+		String hql = "select distinct Task from Task task where task.name like:name or task.description like:description";
+		Query query = session.createQuery(hql);
+		query.setParameter("name", "%"+name+"%");
+		query.setParameter("description", "%"+description+"%");
+		return query.list();
 	}
 
 

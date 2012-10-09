595485fd81f4606990c0c76b4d076520c387befa
Leonardo Wolter <leocwolter@gmail.com>
2012-10-09 16:14:13 -0300

    implementando equals e hashcode

--- src/main/java/br/com/zeng/model/User.java
+++ src/main/java/br/com/zeng/model/User.java
@@ -18,15 +18,8 @@ public class User {
 	
 	private String photo;
 
+	//hibernate eyes only
 	public User() {
-	}
-	
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 
 	public User(String name, String email, String password) {
@@ -36,6 +29,13 @@ public class User {
 
 	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
 
 	public String getName() {
 		return name;
@@ -61,6 +61,24 @@ public class User {
 		this.email = email;
 	}
 
+	public String getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -75,16 +93,17 @@ public class User {
 				return false;
 		} else if (!email.equals(other.email))
 			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
 		return true;
 	}
-
-	public String getPhoto() {
-		return photo;
-	}
-
-	public void setPhoto(String photo) {
-		this.photo = photo;
-	}
-	
 	
 }

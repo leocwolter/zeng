d6a287526b66afe4066278d30c168902773fd81e
leonardowolter <leocwolter@gmail.com>
2012-12-21 14:32:17 -0200

    removing the component annotation(vraptor cant inject xstream)

--- src/main/java/br/com/zeng/serialization/PersistentBagConverter.java
+++ src/main/java/br/com/zeng/serialization/PersistentBagConverter.java
@@ -2,12 +2,9 @@ package br.com.zeng.serialization;
 
 import java.util.Collection;
 
-import br.com.caelum.vraptor.ioc.Component;
-
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.converters.collections.CollectionConverter;
 
-@Component
 public class PersistentBagConverter extends CollectionConverter {
 
 	public PersistentBagConverter(XStream xstream) {

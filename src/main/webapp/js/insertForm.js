c2358e050ecf11a626e82760753cc2a3a0a026aa
leonardowolter <leocwolter@gmail.com>
2013-01-22 15:43:23 -0200

    refactoring more javascript

--- src/main/webapp/js/insertForm.js
+++ src/main/webapp/js/insertForm.js
@@ -49,41 +49,54 @@ $(function () {
 	$(".insert-task-list-form").submit(function (event) {	
 		event.preventDefault();
 		submitForm($(this), function (data) {
-			var taskArea = createTaskArea(data.taskList),
-				category = $(".category", parent.document);
-			$(taskArea).appendTo(category);		
+			var taskArea = new TaskArea(data.taskList);
+			var category = $(".category", parent.document);
+			$(taskArea.element).appendTo(category);		
 			$(".task-list").configureZengTaskList();
 		});
 	});
 
-	function createTaskArea(taskListData) {
-		var taskList = $.nano("<ul class='task-list ui-sortable' data-tasklist-id='{id}'>", taskListData),
-			taskListTitle = $.nano("<h3>{name}</h3>", taskListData),
-			nav = createTaskAreaNavBar(),
-			addTaskButton = $("<a class='add-button add-task-button modal'>+Add Task</a>")
-							.attr("href",context+"project/category/taskList/"+taskListData.id+"/addTaskForm"),
-			taskArea = $("<section class='task-area'>")
-						.append(taskListTitle)
-						.append(nav)
-						.append(taskList)
-						.append(addTaskButton);
+	function TaskArea(taskListData) {
+		var taskList = this.createTaskList(taskListData),
+			taskListTitle = this.createTitle(taskListData),
+			nav = this.createTaskAreaNavBar(),
+			addTaskButton = this.createAddTaskButton(taskListData);
+		
+		this.element = $("<section class='task-area'>")
+			.append(taskListTitle)
+			.append(nav)
+			.append(taskList)
+			.append(addTaskButton);
 
-		return taskArea;
 	}
 	
-	function createTaskAreaNavBar() {
-		var all =  generateMenuItem({"filter":"nofilter","text":"All"});
+	TaskArea.prototype.createAddTaskButton = function (taskListData) {
+		var href= $.nano(context+"project/category/taskList/{id}/addTaskForm", taskListData);
+		var button = $("<a class='add-button add-task-button modal'>+Add Task</a>").attr("href",href);
+		return button;
+	}
+	
+	TaskArea.prototype.createTitle = function (taskListData) {
+		return $.nano("<h3>{name}</h3>", taskListData)
+	}
+	
+	TaskArea.prototype.createTaskList = function (taskListData) {
+		return $.nano("<ul class='task-list ui-sortable' data-tasklist-id='{id}'>", taskListData);
+	}
+	
+	TaskArea.prototype.createTaskAreaNavBar = function () {
+		var all =  this.generateMenuItem({"filter":"nofilter","text":"All"});
 		all.find("a").addClass("task-filter-selected");
 		var menuItems = $("<ul>")
 			.append(all)
-			.append(generateMenuItem({"filter":"todo", "text":"To do"}))
-			.append(generateMenuItem({"filter":"doing", "text":"Doing"}))
-			.append(generateMenuItem({"filter":"done", "text":"Done"}))
-			.append(generateMenuItem({"filter":"mine", "text":"Mine"}));
+			.append(this.generateMenuItem({"filter":"todo", "text":"To do"}))
+			.append(this.generateMenuItem({"filter":"doing", "text":"Doing"}))
+			.append(this.generateMenuItem({"filter":"done", "text":"Done"}))
+			.append(this.generateMenuItem({"filter":"mine", "text":"Mine"}));
 		return $("<nav>").addClass("task-menu-bar").append(menuItems);
 	}
 	
-	function generateMenuItem(data) {
+	TaskArea.prototype.generateMenuItem = function(data) {
 		return $($.nano("<li><a class='task-filter' data-filter='{filter}' href='#'>{text}</a></li>", data));
 	};
 	
@@ -95,7 +108,7 @@ $(function () {
 			var	task = new Task(taskData),
 				selector = $.nano("[data-tasklist-id = {taskList.id}]", taskData),
 				taskList = $(selector, parent.document);
-			$(taskList).append(task.element);
+			$(task.element).appendTo(taskList);
 		});
 	});
 	

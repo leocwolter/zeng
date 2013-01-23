f463bc5a6147a33373564564a7f4cb315d6db72b
leonardowolter <leocwolter@gmail.com>
2013-01-23 12:38:23 -0200

    saving a boolean to keep the sidebar closed when true

--- src/main/webapp/js/screen_size.js
+++ src/main/webapp/js/screen_size.js
@@ -2,34 +2,63 @@
 
 $(document).ready(
 		function() {
-			// Get Width of elements on the load
-			var projectSidebarWidth = $('#project-sidebar').width();
-			var projectSidebarClickAreaWidth = $('#project-sidebar-click-area').width();
+			var SideBar = function(data){
+				console.log("instanciei");
+				this.closedSidebar = data.closedSidebar;
+
+				// Get Width of elements on the load
+				this.element = data.element;
+				this.clickArea = data.clickArea;
+
+				this.setToggleFunctions();
+			}
 			
-			if($('#project-sidebar')[0]){
-				$('.resizable').addClass('opened-sidebar');
+			SideBar.prototype.setToggleFunctions = function(){
+				if(this.closedSidebar == "true"){
+					this.firstToggleFunction = openSideBar;
+					this.secondToggleFunction = closeSideBar;
+					this.element.css('width', this.clickArea.width());
+				}else{
+					this.firstToggleFunction = closeSideBar;
+					this.secondToggleFunction = openSideBar;
+					$('.resizable').addClass('opened-sidebar');
+				}
+
+			}
+			
+			SideBar.prototype.toggle = function(){
+				this.clickArea.toggle(this.firstToggleFunction, this.secondToggleFunction);
 			}
 
-			// Minimize / Maximize Sidebar and readjusts elements class setting
-			$('#project-sidebar-click-area').toggle(
-				function() {
-	
-					$('#project-sidebar').animate({
-						'width' : projectSidebarClickAreaWidth
-					}, function() {
-						$('.resizable').addClass('closed-sidebar').removeClass('opened-sidebar');
-					});
-	
-				}, function() {
-	
-					$('#project-sidebar').animate({
-						'width' : projectSidebarWidth
-					}, function() {
-						$('.resizable').addClass('opened-sidebar').removeClass('closed-sidebar');
-					});
-	
-				}
-			);
 
+			var sideBarElement = $('#project-sidebar'), sideBarWidth = sideBarElement.width();
+			var clickArea = $('#project-sidebar-click-area'), clickAreaWidth = clickArea.width();
+			
+			//Animation to close sidebar
+			var closeSideBar = function(){
+				localStorage.closedSidebar = true;
+				$('#project-sidebar').animate({
+					'width' : clickAreaWidth
+				}, function() {
+					$('.resizable').addClass('closed-sidebar').removeClass('opened-sidebar');
+				});
+			};
+			
+			//Animation to open sidebar
+			var openSideBar = function(){
+				localStorage.closedSidebar = false;
+				$('#project-sidebar').animate({
+					'width' : sideBarWidth
+				}, function() {
+					$('.resizable').addClass('opened-sidebar').removeClass('closed-sidebar');
+				});
+			};
+			
+			var sideBar = new SideBar({
+					closedSidebar: localStorage.closedSidebar,
+					element: sideBarElement,
+					clickArea: clickArea
+			});
+			sideBar.toggle();
 		}
 );
